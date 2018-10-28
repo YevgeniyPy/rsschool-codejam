46f7b6661958e3b585dbc7a86c6531c42f935af0
YevgeniyPy <43146121+YevgeniyPy@users.noreply.github.com>
2018-10-28 20:39:22 +0300

    fix: - a bug test

--- test.js
+++ test.js
@@ -1,11 +1,15 @@
-const assert = sumOfOther('assert');
+const assert = require('assert');
 Object.freeze(assert);
-const getSum = require('./sumOfOther');
+const sumOfOther = require('./sumOfOther');
 
 describe('sumOfOther', () => {
   it('1', () => {
-    const solutions = getSum([2, 3, 4, 1]);
+    const solutions = sumOfOther([2, 3, 4, 1]);
     assert.deepEqual(solutions, [8, 7, 6, 9]);
+  });
+  it('2', () => {
+    const solutions = sumOfOther([3, 4, 2, 6, 7]);
+    assert.deepEqual(solutions, [19, 18, 20, 16,15]);
   });
   
 });
